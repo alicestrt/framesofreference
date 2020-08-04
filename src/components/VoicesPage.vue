d2e4bc1dca4fe56df65359400a484fbfcae723a9
Gill <gillian.elizabeth.baldwin@gmail.com>
2020-08-04 13:03:37 +0200

    changed voices code cleaner

--- src/components/VoicesPage.vue
+++ src/components/VoicesPage.vue
@@ -4,7 +4,7 @@
 
 
 * {
-border: 0px black solid;
+border: 1px black solid;
 }
 .bios {
   padding-top: 50px;
@@ -54,146 +54,146 @@ a:hover {
     <div class="everyonebio">
 
     <b-row>
-        <b-col md="5"> </b-col>
-        <b-col md="2">
+
+        <b-col md="2" offset-md="5">
           <b-img class="circle" center src="media/spiral.png"></b-img>
         </b-col>
-        <b-col md="5"> </b-col>
+
     </b-row>
 
 
     <b-row class="bios">
-        <b-col md="2"> </b-col>
-        <b-col md="8">
+
+        <b-col md="8" offset-md="2">
             <p class="name"> <a href="/RezaNik">Reza Nik</a></p>
             <p> On fear, scaffolding, loosely adaptable infastructures, eyes on the street, architecture, zig zaggy. On fear, scaffolding, loosely adaptable infastructures, eyes on the street, architecture, zig zaggy.</p>
         </b-col>
-        <b-col md="2"> </b-col>
+
     </b-row>
 
 
     <b-row>
-        <b-col md="5"> </b-col>
-        <b-col md="2">
+
+        <b-col md="2" offset-md="5">
           <b-img class="circle" center src="media/spiral.png"></b-img>
         </b-col>
-        <b-col md="5"> </b-col>
-    </b-row>
+
+   </b-row>
 
 
     <b-row class="bios">
-        <b-col md="2"> </b-col>
-        <b-col md="8">
+
+        <b-col md="8" offset-md="2">
           <a href="/RezaNik">  <p class="name">Vera van de Berg</p> </a>
             <p> Machine learning, labeling, technological solutionism.</p>
         </b-col>
-        <b-col md="2"> </b-col>
+
     </b-row>
 
 
     <b-row>
-        <b-col md="5"> </b-col>
-        <b-col md="2">
+
+        <b-col md="2" offset-md="5">
           <b-img class="circle" center src="media/spiral.png"></b-img>
         </b-col>
-        <b-col md="5"> </b-col>
+
     </b-row>
 
 
     <b-row class="bios">
-        <b-col md="2"> </b-col>
-        <b-col md="8">
+
+        <b-col md="8" offset-md="2">
           <a href="/RezaNik">  <p class="name">Emma Fagan</p> </a>
             <p> On fear, scaffolding, loosely adaptable infastructures, eyes on the street, architecture, zig zaggy.</p>
         </b-col>
-        <b-col md="2"> </b-col>
+
     </b-row>
 
     <b-row>
-        <b-col md="5"> </b-col>
-        <b-col md="2">
+
+        <b-col md="2" offset-md="5">
           <b-img class="circle" center src="media/spiral.png"></b-img>
         </b-col>
-        <b-col md="5"> </b-col>
+
     </b-row>
 
 
     <b-row class="bios">
-        <b-col md="2"> </b-col>
-        <b-col md="8">
+
+        <b-col md="8" offset-md="2">
           <a href="/RezaNik">  <p class="name">Luke Murphy</p> </a>
             <p> Start ups, media coverage of tech, encryption, hope, alternative structures of technology.</p>
         </b-col>
-        <b-col md="2"> </b-col>
+
     </b-row>
 
     <b-row>
-        <b-col md="5"> </b-col>
-        <b-col md="2">
+
+        <b-col md="2" offset-md="5">
           <b-img class="circle" center src="media/spiral.png"></b-img>
         </b-col>
-        <b-col md="5"> </b-col>
+
     </b-row>
 
     <b-row class="bios">
-        <b-col md="2"> </b-col>
-        <b-col md="8">
+
+        <b-col md="8" offset-md="2">
           <a href="/RezaNik">  <p class="name">Dorothy Cheung</p> </a>
             <p> Public conversation, awareness, participation in systems, opting out, everyday tracking, differences between countries they marketing versus realities.</p>
         </b-col>
-        <b-col md="2"> </b-col>
+
     </b-row>
 
     <b-row>
-        <b-col md="5"> </b-col>
-        <b-col md="2">
+
+        <b-col md="2" offset-md="5">
           <b-img class="circle" center src="media/spiral.png"></b-img>
         </b-col>
-        <b-col md="5"> </b-col>
+
     </b-row>
 
     <b-row class="bios">
-        <b-col md="2"> </b-col>
-        <b-col md="8">
+
+        <b-col md="8" offset-md="2">
           <a href="/RezaNik">  <p class="name">Joanette van der Ver</p> </a>
             <p> On fear, scaffolding, loosely adaptable infastructures, eyes on the street, architecture, zig zaggy.</p>
         </b-col>
-        <b-col md="2"> </b-col>
+
     </b-row>
 
     <b-row>
-        <b-col md="5"> </b-col>
-        <b-col md="2">
+
+        <b-col md="2" offset-md="5">
           <b-img class="circle" center src="media/spiral.png"></b-img>
         </b-col>
-        <b-col md="5"> </b-col>
+
     </b-row>
 
     <b-row class="bios">
-        <b-col md="2"> </b-col>
-        <b-col md="8">
+
+        <b-col md="8" offset-md="2">
           <a href="/RezaNik">  <p class="name">Francis May</p> </a>
             <p> Government funding, privacy, public health, data gathering, Canada ,Health tracking. </p>
 
         </b-col>
-        <b-col md="2"> </b-col>
+
     </b-row>
 
     <b-row>
-        <b-col md="5"> </b-col>
-        <b-col md="2">
+        <
+        <b-col md="2" offset-md="5">
           <b-img class="circle" center src="media/spiral.png"></b-img>
         </b-col>
-        <b-col md="5"> </b-col>
+
     </b-row>
 
     <b-row class="bios">
-        <b-col md="2"> </b-col>
-        <b-col md="8">
+
+        <b-col md="8" offset-md="2">
           <a href="/RezaNik">  <p class="name">Beatriz Pero Gianni</p> </a>
             <p> CCTV sidewalks, public space, </p>
         </b-col>
-        <b-col md="2"> </b-col>
+        
     </b-row>
 
 
